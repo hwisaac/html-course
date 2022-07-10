2e844ae50dfc740946f3ebc9be2243045efd5a1a
hwisaac <54179672+hwisaac@users.noreply.github.com>
2022-07-10 20:00:44 +0900

    submit 해도 새로고침 안되게 하는 방법
    event.preventDefault(); 를 해서 새로고침 동작을 막는다

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function onLoginSubmit(tomato) {
-    tomato.preventDefault();
+function onLoginSubmit(event) {
+    // preventDefault는 event의 기본행동이든지 발생되지 않도록 막는다.
+    // 토마토 대신 event를 쓴다
+    event.preventDefault(); 
 
-    console.log(tomato);
+    console.log(loginInput.value);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
